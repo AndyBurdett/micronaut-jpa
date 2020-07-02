a43fd1b0ec6a4088b2fa4ccc07a8e7aaf1a3ccef
otaviojava <otaviopolianasantana@gmail.com>
2020-07-02 10:49:42 -0300

    adds temp

--- src/main/java/Temp.java
+++ src/main/java/Temp.java
@@ -5,7 +5,6 @@ public class Temp {
 
     public static void main(String[] args) throws IOException {
         System.out.println("starting");
-        ServerSocket serverSocket = new ServerSocket(9999);
-        System.out.println("test:  " + java.net.InetAddress.getLocalHost());
+        System.out.println("Testing: " + System.getenv("JDBC_USER"));
     }
 }
